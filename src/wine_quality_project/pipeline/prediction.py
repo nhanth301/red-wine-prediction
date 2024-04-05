8684405b5ad220b0ec9b8f21a3e998cecbc9266b
nhanth301 <nhanthien.tnn@gmail.com>
2024-04-05 23:43:23 +0700

    prediction pipeline updated

--- src/wine_quality_project/pipeline/prediction.py
+++ src/wine_quality_project/pipeline/prediction.py
@@ -2,11 +2,17 @@ import joblib
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from wine_quality_project.config.configuration import ConfigurationManager
 
 class PredictionPipeline:
     def __init__(self):
+        self.config = ConfigurationManager()
         self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
+        self.preprocessor = joblib.load(Path('artifacts/data_transformation/preprocessor.joblib'))
 
     def predict(self, data):
-        prediction = self.model.predict(data)
+        cols = list(self.config.schema.COLUMNS.keys())
+        data_df = pd.DataFrame(data, columns=cols[:-1])
+        prediction = self.model.predict(self.preprocessor.transform(data_df))
         return prediction
+
